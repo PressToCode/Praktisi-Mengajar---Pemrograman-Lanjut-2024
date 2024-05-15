651ab318093d09ff82622f863a77e89bd4d8fd19
PressToCode <137992170+PressToCode@users.noreply.github.com>
2024-05-15 12:19:47 +0700

    Update SemuaException.java

--- Java ExceptionHandling/src/main/java/com/mycompany/tugaspraktisi/Latihan/SemuaException.java
+++ Java ExceptionHandling/src/main/java/com/mycompany/tugaspraktisi/Latihan/SemuaException.java
@@ -17,7 +17,7 @@ public class SemuaException {
         try{ 
             System.out.println(a/b); 
         } catch(ArithmeticException e){ 
-            System.out.println("Terjadi Error: " + e);
+            System.out.println("\nTerjadi eksepsi: " + e);
         } finally {
             System.out.println("\nAkhir dari try-catch ArtihmeticException");
         }
@@ -28,11 +28,40 @@ public class SemuaException {
             System.out.println(arr[1]);
             System.out.println("Baris ini tidak pernah dieksekusi");
         }catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("Terjadi eksepsi karena index melebihi kapasitas");
+            System.out.println("\nTerjadi eksepsi: " + e);
         } finally {
             System.out.println("\nAkhir dari try-catch ArrayIndexOutOfBoundsException");
         }
         
+        // ClassNotFoundException
+        try{
+            Class.forName("Class1");
+        } catch(ClassNotFoundException e){
+            System.out.println("\nTerjadi eksepsi: " + e);
+        } finally {
+            System.out.println("\nAkhir dari try-catch ClassNotFoundException");
+        }
         
+        // NullPointerException
+        try { 
+            String A = null; //null
+            System.out.println(A.charAt(0)); 
+        } catch(NullPointerException e) { 
+            System.out.println("\nTerjadi eksepsi: " + e);
+        } finally {
+            System.out.println("\nAkhir dari try-catch NullPointerException");
+        }
+        
+        // StringIndexOutOfBoundsException
+        try { 
+            String B = "Hello";
+            char c = B.charAt(25);
+            System.out.println(c); 
+        } 
+        catch(StringIndexOutOfBoundsException e) { 
+            System.out.println("\nTerjadi eksepsi: " + e);
+        } finally {
+            System.out.println("\nAkhir dari try-catch StringIndexOutOfBoundsException");
+        }
     } 
 }
