fd80b7ef5515f61e9d490c5aec7cec0882c9cddd
PressToCode <137992170+PressToCode@users.noreply.github.com>
2024-05-17 18:00:47 +0700

    Update Latihan.java

--- Java Operation File/src/main/java/com/mycompany/tugaspraktisi/Latihan/Latihan.java
+++ Java Operation File/src/main/java/com/mycompany/tugaspraktisi/Latihan/Latihan.java
@@ -12,8 +12,29 @@ import java.io.File;
  */
 public class Latihan {
     public static void main(String[] args) {
+        // Membuat File
         File file1 = new File("Contoh.txt");
+        
+        try {
+            file1.createNewFile();
+        } catch(IOException e) {
+            System.out.println("Error: " + e);
+        }
+        
         System.out.println(file1.getAbsolutePath());
         System.out.println(file1.exists()); // True jika file sudah dibuat sebelumnya
+        
+        // Membuat Direktori
+        File dir1 = new File("Folder1");
+        System.out.println(dir1.mkdir());
+        File file2 = new File(dir1,"contoh2.txt");
+        System.out.println(file2.getPath());
+        System.out.println(dir1.isDirectory());
+        
+        // Menggunakan Separator untuk Direktori
+        String Folder1 = "data" + File.separator + "save";
+        System.out.println("Creating folder " + Folder1);
+        File dir2 = new File(Folder1);
+        System.out.println(dir2.getAbsolutePath());
     }
 }
